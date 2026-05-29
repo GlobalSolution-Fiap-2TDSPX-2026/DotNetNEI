df9299df526fcad4d92f3db0425e9110a06af151
Anthony Souza <souzaanthony892@gmail.com>
2026-05-28 21:50:16 -0300

    Criando model 'CloseApproach' e aplicando migration

---
+++ Models/CloseApproach.cs
@@ -0,0 +1,42 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using NEI.Models;
+
+namespace NEI;
+
+[Table("DB_CLOSE_APPROACH")]
+public class CloseApproach
+{
+
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("ASTEROID_ID")]
+    public int AsteroidId { get; set; }
+
+    [JsonIgnore]
+    [ForeignKey("AsteroidId")]
+    public Asteroid Asteroid { get; set; }
+
+    [Required]
+    [Column("APPROACH_DATE")]
+    public DateTime ApproachDate { get; set; }
+
+    [Required]
+    [Column("MISS_DISTANCE_KM", TypeName = "NUMBER(18,2)")]
+    public decimal MissDistanceKm { get; set; }
+
+    [Required]
+    [Column("RELATIVE_VELOCITY_KM", TypeName = "NUMBER(18,2)")]
+    public decimal RelativeVelocityKm { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [Column("ORBITING_BODY")]
+    public string OrbitingBody { get; set; }
+
+}
